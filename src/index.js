971eef63da354cb180b3efd3c6ad284aea849aae
Julia Yakutova <julia.yakutova@gmail.com>
2021-03-04 15:03:21 +0300

    Add basic components.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import SwapiService from './services/swapi';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((people) => {
-  people.forEach(p => {
-    console.log(p.name);
-  });
-});
+ReactDOM.render(<App />,
+  document.getElementById('root'));
